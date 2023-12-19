da36a6c9f9477d2f6503764566a0a1621f3be20b
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-12-19 16:44:42 +0330

    chore: dipatch function of middleware where decryption and encryption is called

--- Podcast/config/middlewares.py
+++ Podcast/config/middlewares.py
@@ -8,3 +8,10 @@ from services import SERVICE_CLASSES
 from utils.async_iterator import async_iterator_wrapper as aiwrap
 
 class InternalSecurityMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request, call_next):
+        # decrypte body of internal services requests
+        self.current_request = request
+        await self.decrypte_internal_requests(request)
+        response = await call_next(request)
+        response = await self.encrypte_internal_response(request, response)
+        return response
