a1f8371f337c209137ac522b83daf053016d86d1
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-12-19 16:56:17 +0330

    chore: podcast's episodes endpoint that retrive them from external service and store them if isn't and retrive it's related users interations data.

--- Podcast/api/v1/external_users_api.py
+++ Podcast/api/v1/external_users_api.py
@@ -48,3 +48,31 @@ async def podcast_list(limit:int=1, offset:int=1):
     response_dict['results'] = await results.to_list(None)
     paginated_podcasts = PaginatedPodcastCollection(**response_dict)
     return JSONResponse(paginated_podcasts.dict(), status_code=status.HTTP_200_OK)
+
+
+
+@router.get("/podcast_episode_list/{podcast_id}", response_description="Podcast's episode list")
+async def podcast_episode_list(podcast_id:int, limit:int=1, offset:int=1):
+    podcast_collection = collections['podcast_collection']
+    episode_collection = collections['episode_collection']
+    response = await DjangoPodcastService.get_podcast_episode_list(podcast_id)
+    response_dict = response.json()
+    episode_ids = list(map(lambda item: item['id'], response_dict['results']))
+    episode_models = EpisodeCollection(episodes=response_dict['results'])
+    for episode in episode_models.episodes:
+        episode.episode_podcast = episode.episode_podcast['id']
+    try:
+        await podcast_collection.insert_one(Podcast(**response_dict['results'][0]['episode_podcast']).dict())
+    except (DuplicateKeyError):
+        pass
+    try:
+        res = await episode_collection.insert_many(episode_models.dict()['episodes'], ordered=False)
+    except BulkWriteError as e:
+        pass
+    podcast = await podcast_collection.find_one(filter={"id": podcast_id}, projection={"_id": 0})
+    pipeline = RETRIVE_COMMENTS_PIPELINE
+    pipeline.append({"$match": {"id": {"$in": episode_ids}}})
+    episodes = await episode_collection.aggregate(pipeline).to_list(None)
+    # episodes = await episode_collection.find(filter={"id": {"$in": episode_ids}}, projection={"_id": 0}).to_list(None)
+    return JSONResponse({"podcast": podcast, "episodes": episodes}, status_code=status.HTTP_200_OK)
+
