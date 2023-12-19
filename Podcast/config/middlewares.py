24a77c191e21d64ed63ecca9d227de4479ee4db8
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-12-19 16:45:07 +0330

    chore: decryption fuction that mody request body by changing recieve method of request

--- Podcast/config/middlewares.py
+++ Podcast/config/middlewares.py
@@ -8,6 +8,16 @@ from services import SERVICE_CLASSES
 from utils.async_iterator import async_iterator_wrapper as aiwrap
 
 class InternalSecurityMiddleware(BaseHTTPMiddleware):
+
+    async def decrypte_internal_requests(self, request: Request):
+        recieve_ = await request._receive()
+        if internal_service:= SERVICE_CLASSES.get(request.headers.get("internal-service")):
+            body = recieve_.get("body").decode().strip("\"").encode()
+            recieve_["body"] = internal_service.Encrypt_tools.decrypt_data(body).encode()
+        async def recieve() -> Message:
+            return recieve_
+
+        request._receive = recieve
     async def dispatch(self, request, call_next):
         # decrypte body of internal services requests
         self.current_request = request
