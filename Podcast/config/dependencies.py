7e92ab906d35753ae2b30e2975ce22c946134f60
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-12-19 16:43:23 +0330

    chore: allowed services permission to restrict endpoint to a list of specified services that has their shared keys. if the header be modified the same key requirment won't allow wrong services to rich this endpoint.

--- Podcast/config/dependencies.py
+++ Podcast/config/dependencies.py
@@ -21,3 +21,13 @@ async def get_service(request: Request):
     return True
 
 
+class AllowedServices:
+    def __init__(self, allowed_services:List):
+        self.allowed_services = allowed_services
+
+    async def __call__(self, request:Request):
+        if not request.headers.get("internal-service"):
+            raise HTTPException(detail="Access denied.", status_code=403)
+        if not request.headers.get("internal-service") in self.allowed_services:
+            raise HTTPException(detail=f"This endpoint is restricted to {' ,'.join(self.allowed_services)} services.", status_code=403)
+        return True
