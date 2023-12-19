cfd4185baf9c4b1634bff60427786fa075b00b04
Mahdi-A98 <mahdi.ahmady1998@gmail.com>
2023-12-19 16:06:39 +0330

    chore: main application config

--- Podcast/main.py
+++ Podcast/main.py
@@ -1,3 +1,16 @@
+# In the name of GOD
+
+from fastapi import FastAPI, Depends, Request
+from fastapi.responses import JSONResponse
+from contextlib import asynccontextmanager
+
+
+from api.v1 import external_users_api
+from db.db import collections, databases, sess_db
+from config.middlewares import InternalSecurityMiddleware
+
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     podcast_db = await sess_db()
@@ -9,3 +22,17 @@ async def lifespan(app: FastAPI):
     yield
     collections.clear()
     podcast_db.client.close()
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(external_users_api.router)
+app.add_middleware(InternalSecurityMiddleware)
+
+
+@app.get("/")
+def root():
+    return JSONResponse({"message": "Hello...this is podcast service"}, status_code=200)
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run("main:app", host="localhost", port=8006, log_level="info", reload=True)
+    # uvicorn.run(app, host="localhost", port=8006, log_level="debug")
